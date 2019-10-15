bf8de5359c5c7fffeb0a61ccf6fff6264d5108a4
Silvan Wenk <silvan.wenk@gmail.com>
2019-10-15 15:52:44 +0200

    Add animated canvas

--- app/public/methods.js
+++ app/public/methods.js
@@ -1,3 +1,10 @@
+var frames = 300;
+var currentFrame = 0;
+var dot = { x: 0, y: 90, radius: 5 };
+var ctx = null;
+var canvasWidth = 400;
+var canvasHeight = 400;;
+
 function loadImage() {
   console.log("Load image");
   var request = new XMLHttpRequest();
@@ -29,3 +36,55 @@ function removeButton() {
     button.remove();
   }
 }
+
+function init() {
+  setTimeout(function() {
+    window.requestAnimationFrame(moveDot);
+  }, 100);
+  window.requestAnimationFrame(draw);
+}
+
+function moveDot() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  dot.x += 1;
+  dot.y += 1/3;
+
+  this.drawLine();
+  this.drawDot(dot)
+  currentFrame += 1;
+
+  if( currentFrame == frames ) {
+    currentFrame = 0;
+    dot = { x: 0, y: 90, radius: 5 };
+  }
+
+  window.requestAnimationFrame(moveDot);
+}
+
+function draw() {  
+  ctx = document.getElementById('canvas').getContext('2d');  
+  this.drawLine();
+  this.drawDot(this.dot);
+  window.requestAnimationFrame(draw);
+}
+
+function drawLine() {
+  ctx.beginPath();
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 400, 400);
+
+  ctx.strokeStyle = 'black';
+  ctx.moveTo(0, 100);
+  ctx.lineTo(300, 200);
+  ctx.stroke();
+}
+
+function drawDot(dot) {
+  ctx.beginPath();
+  ctx.arc(dot.x, dot.y, dot.radius, 0, 5 * Math.PI, false);
+  ctx.fillStyle = 'black';
+  ctx.fill();
+}
+
+init();
