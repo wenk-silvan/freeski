51b79c417816754edab57275d20f2196a27d5e7c
Silvan Wenk <silvan.wenk@gmail.com>
2019-11-05 18:30:17 +0100

    Update port to 80

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 router.get('/', function(req,res){  
