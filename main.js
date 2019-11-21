eb02c5a5e8513152f5a52d3f478410a442380eea
wenksi <silvan.wenk@gmail.com>
2019-11-21 09:25:32 +0100

    Update main.js for the use in AWS Linux instance

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
+#!/usr/bin/env node
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = 80;
 
-router.get('/', function(req,res){  
+router.get('/', function(req,res){
   res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
-router.get('/image', function(req,res){  
+router.get('/image', function(req,res){
   res.send("skiing1.jpg");
 });
 
 app.use(express.static(__dirname + '/app/public'));
 app.use('/', router);
- 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+
+app.listen(server_port, function () {
+  console.log("Listening on port " + server_port);
 });
